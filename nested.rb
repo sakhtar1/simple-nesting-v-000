427842fd6bd7c6145b058257cf68311b0c6a6182
Sharmina Akhtar <sakht318@gmail.com>
2017-11-14 14:18:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
- programmer_hash[:yukihiro_matsumoto] = :known_for 
+ programmer_hash[:yukihiro_matsumoto] = :known_for
 
 
 	programmer_hash =
