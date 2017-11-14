6fe1726b655dc2628e6baa53a1331fb6edb0d11e
Sharmina Akhtar <sakht318@gmail.com>
2017-11-14 15:25:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -134,7 +134,8 @@ def adding_to_dennis
      }
 
 		 dennis_ritchie = programmer_hash[:dennis_ritchie]
-		 dennis_ritchie[:languages] = ["Assembly"]
+		 dennis_ritchies_language= dennis_ritchie[:languages] 
+		 dennis_ritchies_language << ["Assembly"]
 
 
 end
