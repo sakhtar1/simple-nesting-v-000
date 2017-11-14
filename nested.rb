3d2ef1a986db56e9c59b2c1242194a13385b050c
Sharmina Akhtar <sakht318@gmail.com>
2017-11-14 14:22:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -64,8 +64,11 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
- programmer_hash[:yukihiro_matsumoto] = :known_for
-
+	hash = :yukihiro_matsumoto => {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+	}
+	programmer_hash.merge(hash)
 
 	programmer_hash =
  		{
