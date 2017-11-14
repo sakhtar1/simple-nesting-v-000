9a222ecc2529b01709ba6d7479b8002615f40407
Sharmina Akhtar <sakht318@gmail.com>
2017-11-14 14:55:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -64,8 +64,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-	programmer_hash[:yukihiro_matsumoto]=
-	[:known_for]
+	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
+	programmer_hash[:yukihiro_matsumoto][:languages] = "C"
+	
 
 
 
