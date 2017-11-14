4bdbe55870a8be59a4a85cd6fefe1a43593dad32
Sharmina Akhtar <sakht318@gmail.com>
2017-11-14 14:55:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,7 @@ def adding_matz
 	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 	programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
 	programmer_hash[:yukihiro_matsumoto][:languages] = "C"
-	
+	return programmer_hash
 
 
 
